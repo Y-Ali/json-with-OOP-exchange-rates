9197d677e25a67f60e900fdd242b0db7488a023b
Yousuf Ali <yousuf-123@hotmail.co.uk>
2019-05-02 15:45:57 +0100

    started json with oop; exchange_rates

--- run_exchange_oop_json.py
+++ run_exchange_oop_json.py
@@ -3,5 +3,6 @@ from exchange_oop_json import RatesPasser
 rates_obj = RatesPasser('exchange_rate.json')
 
 for key in rates_obj.rates:
-
+    #if key == "BRL":
     print(key, rates_obj.rates[key])
+
